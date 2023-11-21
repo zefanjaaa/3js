46fc56b4b6f2f78bbb29719db9a7cdfa045d9b39
Zefanja Timorason <z.timorason@gmail.com>
2023-11-21 15:01:04 +0100

    added the assets & created a loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,7 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         HELLO
@@ -5,7 +9,18 @@
 }
 
 const Home = () => {
-  return <section className="w-full h-screen relative"></section>;
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 };
 
 export default Home;
